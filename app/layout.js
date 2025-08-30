dfbbed258eedeef999f1f31c97bdd871cdb547e9
Amritpal <Amritpalonly13571@gmail.com>
2025-08-30 09:19:19 +0530

    rollback from pwa

---
+++ app/layout.js
@@ -0,0 +1,37 @@
+/** @format */
+
+import { Geist, Geist_Mono, Roboto_Mono } from "next/font/google";
+import "./globals.css";
+
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
+  subsets: ["latin"],
+});
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "Amrit's Portfolio",
+  description: "Welcome to Amrit's Portfolio - A showcase of projects, skills, and experiences in web development and design.",
+  icon: './favicon.ico'
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased text-white bg-[#202021]`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
