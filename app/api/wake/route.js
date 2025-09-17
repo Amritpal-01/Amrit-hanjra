fefe7d74d5ed01c671c2891f1d63521e65dc3794
Amritpal <Amritpalonly13571@gmail.com>
2025-09-17 19:21:04 +0530

    .

--- app/api/wake/route.js
+++ app/api/wake/route.js
@@ -2,14 +2,13 @@ import { NextResponse } from 'next/server';
 
 export const GET = async (req) => {
   try {
+    const res = await fetch('https://crackit-k24x.onrender.com/wake', {
+      method: 'GET'
+    });
 
-    fetch('https://crackit-k24x.onrender.com/wake', {
-        method : 'GET'
-    })
-
-  } catch {
-    return NextResponse.json({ status: 400, message: "Something Went Wrong" })
+    return NextResponse.json({ status: 200 });
+  } catch (error) {
+    console.error('Error:', error);
+    return NextResponse.json({ status: 400, message: "Something Went Wrong" });
   }
-
-  return NextResponse.json({ status: 200, message: "Message Sent Successfully" })
 }
